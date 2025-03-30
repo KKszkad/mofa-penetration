2953bddfc4087c81a16a831da4d8927b701590c3
KKszkad <2466056107@qq.com>
2025-03-30 15:50:28 +0800

    Complete the check_content and conclude_result

--- python/agent-hub/check_content/check_content/main.py
+++ python/agent-hub/check_content/check_content/main.py
@@ -1,5 +1,8 @@
 from mofa.agent_build.base.base_agent import MofaAgent, run_agent
 
+from openai import OpenAI
+import json
+
     # outputs:
     # # - web_page_content
     # - deduction
@@ -8,8 +11,54 @@ from mofa.agent_build.base.base_agent import MofaAgent, run_agent
     #   plan: make_plan/plan
     #   web_page_outline: search_outline/web_page_outline
 
-def process():
-    return ['推断信息','网页内容']
+def process(query, plan_str, web_page_outline_str):
+    web_page_outline_json = json.loads(web_page_outline_str)
+    web_page_outline = web_page_outline_json['outlines']
+    plan_json = json.loads(plan_str)
+    num_reference = 4
+
+    system_prompt = f"""
+    现在用户向搜索引擎输入了[{query}]，我们对用户的思路进行了分析({plan_json['schedule']})，
+    并增加了搜索词：{plan_json['search_words']}。
+    请结合上述目标和主题，以及下述的网页概览信息，选择最贴合主题的{num_reference}个网页,避免广告。
+    ——————
+    {web_page_outline}
+    ——————
+    分析完成后，你应当只输出符合条件的网页在网页概览信息中的位次，使用空格分开。
+    ——————
+    （假设第1，3，5，7，12个概览为最优选择）
+    示例输出：
+    1 3 5 7 12
+    """
+
+    client = OpenAI(api_key="sk-7a27f1c4d52c46409ddcc83418d59f0a", base_url="https://api.deepseek.com")
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {
+            "role": "system", "content":  system_prompt},
+            {"role": "user", "content": "帮助用户选择最相关的网页"},
+        ],
+        stream=False
+    )
+
+    content = response.choices[0].message.content
+    #TODO 下一步：确定好传输回来的数据
+    num_str_list = content.split()
+    indexes = [int(num) for num in num_str_list]    #得到目标outline的索引
+
+    #TODO 后续用 目前回传的deduction和web_page_content都是 选定的网页概览信息
+    # # 获取确定的结果url
+    # urls = []
+    # for index in indexes:
+    #     urls.append(web_page_outline[index - 1]['url'])
+
+    #TODO 后续爬虫完成后替换真正的deduction 和 web_page_content
+    result = []
+    for index in indexes:
+        result.append(web_page_outline[index-1])
+    
+    return result
 
 @run_agent
 def run(agent:MofaAgent):
@@ -18,12 +67,15 @@ def run(agent:MofaAgent):
     web_page_outline = agent.receive_parameter('web_page_outline')
     # TODO: 在下面添加你的Agent代码,其中agent_inputs是你的Agent的需要输入的参数
     agent_output_name = ['deduction','web_page_content']
-    deduction = process()[0]
-    web_page_content = process()[1]
+
+    #TODO 后续爬虫完成后区分两者
+    result = process(query, plan, web_page_outline)
+    deduction = result
+    web_page_content = result
+
     agent.send_output(agent_output_name=agent_output_name[0],agent_result=deduction)
     agent.send_output(agent_output_name=agent_output_name[1],agent_result=web_page_content)
-    web_page_content
-    agent.send_output()
+
 def main():
     agent = MofaAgent(agent_name='check-content')
     run(agent=agent)
--- python/agent-hub/conclude_result/conclude_result/main.py
+++ python/agent-hub/conclude_result/conclude_result/main.py
@@ -1,5 +1,6 @@
 from mofa.agent_build.base.base_agent import MofaAgent, run_agent
-
+from openai import OpenAI
+import json
     # outputs:
     # - conclusion
     # inputs:
@@ -7,8 +8,27 @@ from mofa.agent_build.base.base_agent import MofaAgent, run_agent
     #   plan: make_plan/plan
     #   web_page_content: check_content/web_page_content
 
-def process():
-    return '最终的响应'
+def process(query, plan_str, web_page_content_str):
+    web_page_content_json = json.loads(web_page_content_str)    
+    plan_json = json.loads(plan_str)
+
+    system_prompt = f"""
+    用户正在搜索结果，我们对用户的思路进行了分析({plan_json['schedule']})，
+    并增加了搜索词：{plan_json['search_words']}。
+    请结合上述目标和主题，以及下述的网页信息。给予用户回答。
+    {web_page_content_json}
+    """
+    client = OpenAI(api_key="sk-7a27f1c4d52c46409ddcc83418d59f0a", base_url="https://api.deepseek.com")
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {
+            "role": "system", "content":  system_prompt},
+            {"role": "user", "content": query},
+        ],
+        stream=False
+    )
+    return response.choices[0].message.content
 
 @run_agent
 def run(agent:MofaAgent):
@@ -18,7 +38,7 @@ def run(agent:MofaAgent):
 
     # TODO: 在下面添加你的Agent代码,其中agent_inputs是你的Agent的需要输入的参数
     agent_output_name = 'conclusion'
-    conclusion = process()
+    conclusion = process(query, plan, web_page_content)
     agent.send_output(agent_output_name=agent_output_name,agent_result=conclusion)
 def main():
     agent = MofaAgent(agent_name='conclude_result')
